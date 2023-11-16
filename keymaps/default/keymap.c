bc9dad159e98b09fe22915481deba71acb48186d
Bastian Reuther <bastir25@icloud.com>
2023-11-16 18:07:49 +0100

    Refactoring

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -4,16 +4,16 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT_ortho_3x4(
-        KC_1, KC_2, KC_3, KC_PLUS,
+    [0] = LAYOUT(
+        KC_1, KC_2, KC_3, KC_AUDIO_MUTE,
         KC_4, KC_5, KC_6, KC_MINUS,
-        KC_7, KC_8, KC_9, KC_AUDIO_MUTE
+        KC_7, KC_8, KC_9, KC_PLUS
     )
 };
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
-    [0] = {ENCODER_CCW_CW(KC_AUDIO_VOL_UP, KC_AUDIO_VOL_DOWN)},
+    [0] = {ENCODER_CCW_CW(KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP)},
 
 };
 #endif
